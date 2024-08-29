9976b6db0551ac0ebebb12e95bfdafe8d4dd5b64
mert-kurttutan <kurttutan.mert@gmail.com>
2024-08-29 20:11:26 +0200

    Fix error in prepare.sh script

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,4 +3,5 @@ sudo apt install -y python3.12-venv
 sudo apt install -y gcc
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source scripts/install_mkl.sh
-source set_env/install_mkl.sh
+source scripts/set_env.sh
+. "$HOME/.cargo/env"
