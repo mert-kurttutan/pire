f29ddf9649d2e9a519625f66eb968d94c9bf3560
mert-kurttutan <kurttutan.mert@gmail.com>
2024-09-16 22:42:52 +0200

    Update scan_cache_params script - This is faster instead of runnign criterion - works well enough that we dont need to criterion - use criterion for more careful experimentation

--- scripts/scan_cache_params.sh
+++ scripts/scan_cache_params.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
 # Define arrays of literal values
-nc_array=(128 192 256 320 384 448)
-kc_array=(128 192 256 320 384 448)
-
-mr=16
-nr=4
-
-# export GLARE_SGEMM_MR=$mr
-# export GLARE_SGEMM_NR=$nr
+nc_array=(192 256 320 384 448 512 640 704 768)
+kc_array=(384 512 576 640 704 768 832 960 1024)
 
 # Outer loop
 for n_i in "${nc_array[@]}"; do
     # Inner loop
     for k_i in "${kc_array[@]}"; do
         # Set environment variables x and y
-        export NC=$n_i
-        export KC=$k_i
+        export GLARE_NC=$n_i
+        export GLARE_KC=$k_i
         
         # echo nc and kc to out.txt
-        echo "nc: $NC, kc: $KC" >> out.txt
-        taskset -c 0 ./target/release/bench --m 4800 --n 4800 --k 4800 --n-repeats 2 --t-layout nt  --bench-type gemm_s16s16s32 --backend corenum >> out.txt
+        echo "nc: $GLARE_NC, kc: $GLARE_KC" >> out.txt
+        ./target/release/bench --m 4800 --n 4800 --k 4800 --t-layout nt --bench-type dgemm --backend glare >> out.txt
+        ./target/release/bench --m 4800 --n 4800 --k 4800 --t-layout nt --bench-type dgemm --backend mkl >> out.txt
+        echo "-----------------------------------------" >> out.txt
         sleep 1
     done
 done
