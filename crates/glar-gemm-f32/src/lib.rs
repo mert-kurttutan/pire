db485c18ce711bc227291148edc1edc25a41222c
mert-kurttutan <kurttutan.mert@gmail.com>
2024-10-01 05:34:36 +0200

    Apply name changes to env var

--- crates/glar-gemm-f32/src/lib.rs
+++ crates/glar-gemm-f32/src/lib.rs
@@ -59,9 +59,9 @@ pub(crate) unsafe fn store_buf(c: *mut TC, c_rs: usize, c_cs: usize, c_buf: &[TC
 
 #[inline(always)]
 fn get_mcnckc() -> (usize, usize, usize) {
-    // let mc = std::env::var("GLARE_MC").unwrap_or("4800".to_string()).parse::<usize>().unwrap();
-    // let nc = std::env::var("GLARE_NC").unwrap_or("192".to_string()).parse::<usize>().unwrap();
-    // let kc = std::env::var("GLARE_KC").unwrap_or("768".to_string()).parse::<usize>().unwrap();
+    // let mc = std::env::var("GLAR_MC").unwrap_or("4800".to_string()).parse::<usize>().unwrap();
+    // let nc = std::env::var("GLAR_NC").unwrap_or("192".to_string()).parse::<usize>().unwrap();
+    // let kc = std::env::var("GLAR_KC").unwrap_or("768".to_string()).parse::<usize>().unwrap();
     // return (mc, nc, kc);
     let (mc, nc, kc) = match (*RUNTIME_HW_CONFIG).hw_model {
         HWModel::Skylake => (4800, 192, 512),
