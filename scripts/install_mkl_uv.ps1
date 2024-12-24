d86f3fe31490e60b6c8cbcf4ebcfe85564623ef8
mert-kurttutan <kurttutan.mert@gmail.com>
2024-12-24 07:28:59 +0100

    Uv (#48)
    * Start new uv based mkl installation
    
    * Active uv after installation
    
    * Fix typo in file name

---
+++ scripts/install_mkl_uv.ps1
@@ -0,0 +1,2 @@
+$env:UV_PROJECT_ENVIRONMENT="../$current_dir/.venv"
+uv sync
--- scripts/prepare.ps1
+++
@@ -1,7 +0,0 @@
-# run scripts/prepare.ps1
-
-$current_directory = Get-Location
-# provide current directory as 1st argument
-. .\scripts\install_mkl.ps1 $current_directory
-# run set_env.ps1
-. .\scripts\set_env.ps1 $current_directory
