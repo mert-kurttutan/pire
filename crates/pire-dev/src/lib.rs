5a2bad86da889a0e783aa780b24b5a943662c5ba
mert-kurttutan <kurttutan.mert@gmail.com>
2024-12-24 07:21:56 +0100

    Start new uv based mkl installation

--- crates/pire-dev/src/lib.rs
+++ crates/pire-dev/src/lib.rs
@@ -192,25 +192,6 @@ type GEMM_I16_FN_TYPE = unsafe extern "C" fn(
 
 const PROJECT_DIR: &str = core::env!("CARGO_MANIFEST_DIR");
 
-// TODO: Add more reasonalble deafult paths for different os,s windows/unix
-// pub static CBLAS_LIBRARY: Lazy<libloading::Library> = Lazy::new(|| unsafe {
-//     #[cfg(target_os = "windows")]
-//     let default_mkl_path = format!("{PROJECT_DIR}/../../.env/Library/bin/mkl_rt.2.dll");
-//     #[cfg(target_os = "linux")]
-//     let default_mkl_path = format!("{PROJECT_DIR}/../../.env/lib/libmkl_rt.so.2");
-//     let mkl_path = std::env::var("PIRE_MKL_PATH").unwrap_or(default_mkl_path);
-//     libloading::Library::new(mkl_path).unwrap()
-// });
-
-// pub static CBLAS_LIBRARY_OPENBLAS: Lazy<libloading::Library> = Lazy::new(|| unsafe {
-//     #[cfg(target_os = "windows")]
-//     let default_openblas_path = format!("{PROJECT_DIR}/../../openblas/openblas.dll");
-//     #[cfg(target_os = "linux")]
-//     let default_openblas_path = format!("{PROJECT_DIR}/../../openblas/libopenblas.so");
-//     let openblas_path = std::env::var("PIRE_OPENBLAS_PATH").unwrap_or(default_openblas_path);
-//     libloading::Library::new(openblas_path).unwrap()
-// });
-
 pub static CBLAS_LIBRARY: Lazy<libloading::Library> = Lazy::new(|| unsafe {
     #[cfg(target_os = "windows")]
     let default_cblas_path = format!("{PROJECT_DIR}/../../.env/Library/bin/mkl_rt.2.dll");
---
+++ scripts/instal_mkl_uv.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install -y make
+sudo apt install -y gcc
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
+
+current_dir=$(pwd)
+curl -LsSf https://astral.sh/uv/install.sh | sh
+export UV_PROJECT_ENVIRONMENT="../$current_dir/.venv"
+uv sync
